29572bfe3aced6e44bcf2d6168eb8d55a1ac9bc8
dennismolund <43784882+dennismolund@users.noreply.github.com>
2019-02-08 11:45:26 +0100

    Created basic square CalenderView.

--- app/src/main/java/mode1719/student/ju/calendar/MainActivity.java
+++ app/src/main/java/mode1719/student/ju/calendar/MainActivity.java
@@ -3,11 +3,35 @@ package mode1719.student.ju.calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.squareup.timessquare.CalendarPickerView;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Date currentday = new Date();
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 3);
+
+        CalendarPickerView datePicker = findViewById(R.id.calendarView);
+        datePicker.init(currentday, nextYear.getTime()).withSelectedDate(currentday);
+
+        datePicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(Date date) {
+
+            }
+
+            @Override
+            public void onDateUnselected(Date date) {
+
+            }
+        });
     }
 }
